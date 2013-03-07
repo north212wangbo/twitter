52533f28a0e9e153bf750efabf7aabd39748fcec
Bo Wang <bo.wang@ecs222-7.labs.encs>
2013-03-07 11:32:19 -0800

    Initial Commit

---
+++ Twitter/TwitterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TwitterAppDelegate.h
+//  Twitter
+//
+//  Created by Bo Wang on 3/7/13.
+//  Copyright (c) 2013 Bo Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwitterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Twitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Twitter
+//
+//  Created by Bo Wang on 3/7/13.
+//  Copyright (c) 2013 Bo Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TwitterAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TwitterAppDelegate class]));
+    }
+}
